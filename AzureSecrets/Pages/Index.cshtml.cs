ac87f119823caa644b4eb969fd09d5ba79472831
Pablo Trujillo <Pablo.TrujilloAvendano@tajamar365.com>
2024-04-24 18:42:00 +0200

    Agregar archivos de proyecto.

---
+++ AzureSecrets/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureSecrets.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Message"] = "My key val= " + _configuration["SQLPassword"];
+        }
+    }
+}
